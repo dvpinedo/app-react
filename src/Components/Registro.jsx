d320c6f25429012c927928d1275e621bc4e19cfe
dvpinedo <dv.jpinedo@gmail.com>
2022-04-25 00:16:10 -0500

    segundo

--- src/Components/Registro.jsx
+++ src/Components/Registro.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {nanoid} from 'nanoid'
-import { firebase } from '../firebase'
+import {firebase} from '../firebase'
 const Registro = () => {
     const [nombre,setNombre]=React.useState('')
     const [apellido,setApellido]=React.useState('')
@@ -12,47 +12,79 @@ const Registro = () => {
     const [id, setId]=React.useState('')
 
     const [edicion, setEdicion]=React.useState(false)
+    const [error, setError]=React.useState(null)
     const [listaEstudiantes,setListaEstudiantes]=React.useState([])
 
     const db = firebase.firestore();
 
-    const guardarEstudiantes = (e)=>{
+    React.useEffect(()=>{
+        const obtenerdatos= async() =>{
+            try{
+                const db = firebase.firestore()
+                const data = await db.collection('students').get()
+                const arrayData=data.docs.map (item =>(
+                    {
+                        id: item.id, ... item.data()
+                    }
+                ))
+                //console.log(arrayData)
+                setListaEstudiantes(arrayData)
+            }catch(error){
+                console.log(error)
+            }
+        }
+        obtenerdatos()
+    },[])
+
+    const guardarEstudiantes = async (e)=>{
         e.preventDefault()
 
         if(!nombre.trim()){
-            alert("Digite el estudiante")
+            alert("Digite el nombre")
             return
         }
         if(!apellido.trim()){
-            alert("Digite el estudiante")
+            alert("Digite el apellido")
             return
         }
         if(!correo.trim()){
-            alert("Digite el estudiante")
+            alert("Digite el correo")
             return
         }
         if(!direccion.trim()){
-            alert("Digite el estudiante")
+            alert("Digite la direccion")
             return
         }
         if(!telefono.trim()){
-            alert("Digite el estudiante")
+            alert("Digite el telefono")
             return
         }
         if(!ciudad.trim()){
-            alert("Digite el estudiante")
+            alert("Digite la ciudad")
             return
         }
         if(!programa.trim()){
-            alert("Digite el estudiante")
+            alert("Digite el programa")
             return
         }
 
-        setListaEstudiantes([
-            ... listaEstudiantes,
+        try{
+            const db = firebase.firestore();
+            const student = {
+            nombreEstudiante: nombre,
+            apellidoEstudiante: apellido,
+            correoEstudiante: correo,
+            direccionEstudiante: direccion,
+            telefonoEstudiante: telefono,
+            ciudadEstudiante: ciudad,
+            programaEstudiante: programa
+          }
+          const data= await db.collection('students').add(student)
+            setListaEstudiantes([
+            ...listaEstudiantes,
             {id:nanoid(),nombreEstudiante: nombre, apellidoEstudiante: apellido, correoEstudiante: correo, direccionEstudiante: direccion, telefonoEstudiante: telefono, ciudadEstudiante: ciudad, programaEstudiante: programa}
         ])
-
+        console.log(listaEstudiantes)
         e.target.reset()
 
         setNombre("")
@@ -62,6 +94,12 @@ const Registro = () => {
         setTelefono("")
         setCiudad("")
         setPrograma("")
+        setError(null)
+
+        }catch(error){
+            console.log(error)
+        }
+        
     }
     
     const editar= item =>{
@@ -73,13 +111,86 @@ const Registro = () => {
         setCiudad(item.ciudadEstudiante)
         setPrograma(item.programaEstudiante)
         setEdicion(true)
+        setId(item.id)
     }
-    const editarEstudiantes = e=>{
+    const editarEstudiantes = async e=>{
         e.preventDefault()
-        const arrayEditado = listaEstudiantes.map(
-        item => item.id===id ? {id:id, nombreEstudiante: nombre, apellidoEstudiante: apellido, correoEstudiante:correo, direccionEstudiante: direccion, telefonoEstudiante:telefono, ciudadEstudiante: ciudad, programaEstudiante: programa} : item
-)
-        setListaEstudiantes(arrayEditado)
+
+        
+        if(!nombre.trim()){
+            alert("Digite el nombre")
+            return
+        }
+        if(!apellido.trim()){
+            alert("Digite el apellido")
+            return
+        }
+        if(!correo.trim()){
+            alert("Digite el correo")
+            return
+        }
+        if(!direccion.trim()){
+            alert("Digite la direccion")
+            return
+        }
+        if(!telefono.trim()){
+            alert("Digite el telefono")
+            return
+        }
+        if(!ciudad.trim()){
+            alert("Digite la ciudad")
+            return
+        }
+        if(!programa.trim()){
+            alert("Digite el programa")
+            return
+        }
+
+        try{ console.log(id)
+            const db = firebase.firestore()
+            await db.collection('students').doc(id).update({
+            nombreEstudiante: nombre,
+            apellidoEstudiante: apellido,
+            correoEstudiante: correo,
+            direccionEstudiante: direccion,
+            telefonoEstudiante: telefono,
+            ciudadEstudiante: ciudad,
+            programaEstudiante: programa
+            })
+
+            
+            const arrayEditado = listaEstudiantes.map(
+                item => item.id===id ? {id:id, nombreEstudiante: nombre, apellidoEstudiante: apellido, correoEstudiante:correo, direccionEstudiante: direccion, telefonoEstudiante:telefono, ciudadEstudiante: ciudad, programaEstudiante: programa} : item
+        )
+                setListaEstudiantes(arrayEditado)
+                setNombre('')
+                setApellido('')
+                setCorreo('')
+                setDireccion('')
+                setTelefono('')
+                setCiudad('')
+                setPrograma('')
+                setId('')
+                setEdicion(false)
+                setError(null)
+        }catch(error){
+            console.log(error)
+        }
+    
+    }
+    const eliminar = async id =>{
+        try{
+            const db = firebase.firestore()
+            await db.collection('students').doc(id).delete()
+            const aux= listaEstudiantes.filter(item=> item.id !== id)
+            setListaEstudiantes(aux)
+        }catch(error){
+            console.log(error)
+        }
+  
+    }
+    const cancelar = () =>{
+        setEdicion(false)
         setNombre('')
         setApellido('')
         setCorreo('')
@@ -88,23 +199,9 @@ const Registro = () => {
         setCiudad('')
         setPrograma('')
         setId('')
-        setEdicion(false)
-    }
-    const eliminar = id =>{
-        const aux= listaEstudiantes.filter(item=> item.id != id)
-        setListaEstudiantes(aux)
-    }
+        setError(null)
 
-    const student = {
-        name: "Bart Simpson",
-        age: 18,
-      };
-    
-      const addStudent = async (student) => {
-        const data = await db.collection("students").add(student);
-        console.log(data)
-      };
-    
+    }
 
   return (
     <div className='container mt-5'>
@@ -117,7 +214,7 @@ const Registro = () => {
                     {
                 listaEstudiantes.map((item)=>(
                 <li className="list-group-item" key={item.id}>
-                <span className='lead'>{item.nombreEstudiante}-{item.apellidoEstudiante}-{item.correoEstudiante}-{item.direccionEstudiante}-{item.telefonoEstudiante}-{item.ciudadEstudiante}-{item.programaEstudiante}</span>
+                <span className='lead'>{item.nombreEstudiante} {item.apellidoEstudiante}-{item.correoEstudiante}-{item.direccionEstudiante}-{item.telefonoEstudiante}-{item.ciudadEstudiante}-{item.programaEstudiante}</span>
                 <button className='btn btn-danger btn-sm float-end mx-2'onClick={()=>eliminar(item.id)}>Eliminar</button>
                 <button className='btn btn-warning btn-sm float-end' onClick={()=>editar(item)}>Editar</button>
                 </li>
@@ -132,7 +229,7 @@ const Registro = () => {
                     edicion ? 'Editarndo datos' : 'Ingresando datos'
                 }
                 <br />
-                <form onSubmit={edicion? editarEstudiantes :guardarEstudiantes}>
+                <form onSubmit={edicion? editarEstudiantes : guardarEstudiantes}>
 
                 <input className='form-control mb-2' type="text" placeholder='Ingrese su nombre'onChange={(e)=>setNombre(e.target.value)}value={nombre}/>
                 <input className='form-control mb-2'type="text" placeholder='Ingrese su apellido'onChange={(e)=>setApellido(e.target.value)}value={apellido}/>
@@ -142,11 +239,17 @@ const Registro = () => {
                 <input className='form-control mb-2' type="text" placeholder='Ingrese su ciudad' onChange={(e)=>setCiudad(e.target.value)}value={ciudad}/>
                 <input className='form-control mb-2' type="text" placeholder='Ingrese su programa' onChange={(e)=>setPrograma(e.target.value)}value={programa}/>
                 <br />
-                <button className='btn btn-primary btn-block'>Crear estudiante</button>
-            
-
+                {
+                    edicion ? (
+                    <>
+                    <button className='btn btn-warning btn-block' type='submit'>Editar</button>
+                    <button className='btn btn-dark btn-block mx-2'onClick={()=>cancelar()}>Cancelar</button>
+                    </>
+                    )
+                    :
+                <button className='btn btn-primary btn-block' type='submit'>Crear estudiante</button>
+                }
                 </form>
-                <button onClick={() => addStudent(student)}>Add person</button>
             </div>
         </div>
     </div>
@@ -154,3 +257,4 @@ const Registro = () => {
 }
 
 export default Registro
+export {firebase}
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8uR9DgyZNhRWS0QtXW2UDMGDc6q3WD2g",
-    authDomain: "demoweb1-17dd3.firebaseapp.com",
-    projectId: "demoweb1-17dd3",
-    storageBucket: "demoweb1-17dd3.appspot.com",
-    messagingSenderId: "714087629679",
-    appId: "1:714087629679:web:2976eb0653b96266229e9a",
-    measurementId: "G-VFZV0G5S1D"
+  apiKey: "AIzaSyAjUQ-M37XYDWLGW4HvtcphOKKXs8-lKgE",
+  authDomain: "app-react-51fe1.firebaseapp.com",
+  projectId: "app-react-51fe1",
+  storageBucket: "app-react-51fe1.appspot.com",
+  messagingSenderId: "1012876227072",
+  appId: "1:1012876227072:web:ae1f3845203a82c3341ee5",
+  measurementId: "G-6CQ75CPKH7"
   };
   
   firebase.initializeApp(firebaseConfig)
